6573fc396c3a5c33cd37329e35795134b6a47795
Igor Svistun <igorekha2003@icloud.com>
2021-03-06 01:06:46 +0300

    DZ1

---
+++ HomeWork/DZ1/VK_api.py
@@ -0,0 +1,18 @@
+import requests
+from pprint import pprint
+import json
+
+url = 'https://api.vk.com/method/groups.get'
+params = {
+    'user_id': '375232192',
+    'extended': '1',
+    'v': '5.130',
+    'access_token': 'f31ca259862bcc94b9b796b35f916c812cbc36efe11142a7dfb47f0bbfc50221ef72ba8b6a6f5a8df613e'
+}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+    'Accept': '*/*'
+}
+response = requests.get(url, params=params, headers=headers)
+with open('VK_api.json', 'w') as f:
+    json.dump(response.json(), f, indent=4)
