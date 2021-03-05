30954d25afe3bd357d3e96f1a2fbe51faf07b022
Igor Svistun <igorekha2003@icloud.com>
2021-03-06 01:11:51 +0300

    'DZ1fix'

--- HomeWork/DZ1/VK_api.py
+++ HomeWork/DZ1/VK_api.py
@@ -7,7 +7,6 @@ params = {
     'user_id': '375232192',
     'extended': '1',
     'v': '5.130',
-    'access_token': 'f31ca259862bcc94b9b796b35f916c812cbc36efe11142a7dfb47f0bbfc50221ef72ba8b6a6f5a8df613e'
 }
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
