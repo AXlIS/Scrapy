f174b7bacf071313f87341ae297b641241a2fec7
Igor Svistun <igorekha2003@icloud.com>
2021-03-08 08:55:09 +0300

    'start'

--- HomeWork/DZ1/VK_api.py
+++ HomeWork/DZ1/VK_api.py
@@ -5,6 +5,7 @@ import json
 url = 'https://api.vk.com/method/groups.get'
 params = {
     'user_id': '375232192',
+    'access_token': 'f31ca259862bcc94b9b796b35f916c812cbc36efe11142a7dfb47f0bbfc50221ef72ba8b6a6f5a8df613e',
     'extended': '1',
     'v': '5.130',
 }
@@ -15,3 +16,4 @@ headers = {
 response = requests.get(url, params=params, headers=headers)
 with open('VK_api.json', 'w') as f:
     json.dump(response.json(), f, indent=4)
+pprint(response.json())
